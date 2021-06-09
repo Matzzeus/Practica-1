7c2b57c6952fb8ead4a5f12698df6420d4cad996
Mario Tzul <marioajpop201931177@cunoc.edu.gt>
2021-06-09 15:49:58 -0600

    ejercicio1Java

---
+++ ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ejercicio1{
+
+public static void main(String[] args) {
+    int n1, n2, n3;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("ingrese un numero: ");
+    n1 = scanner.nextInt();
+    System.out.print("ingrese un numero: ");
+    n2 = scanner.nextInt();
+
+    n3 = n1 + n2;
+
+
+        System.out.println("hola el resultado es: " + n3);
+       
+    }
+}
