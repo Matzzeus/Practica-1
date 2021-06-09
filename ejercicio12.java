ecd876c5cf9220b31cc2565fdf9f220add7bd4ac
Mario Tzul <marioajpop201931177@cunoc.edu.gt>
2021-06-09 16:00:01 -0600

    12Java

---
+++ ejercicio12.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ejercicio12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double t1, conv;
+        System.out.print("ingrese una temperatura Celsius: ");
+        t1 = scanner.nextInt();
+        conv = ((t1 * 9/5) + 32);
+        System.out.print("la temperatura en : " + conv + " Fahrenheit");
+
+
+
+
+
+    }
+}
